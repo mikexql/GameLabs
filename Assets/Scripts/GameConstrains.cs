ee5703acf138c16f0789bb6fadc64775d01e5bc6
EMKwasam <samuelkwa1@gmail.com>
2025-10-16 22:55:09 +0800

    Completed HUD prefab

---
+++ Assets/Scripts/GameConstrains.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+[CreateAssetMenu(fileName =  "GameConstants", menuName =  "ScriptableObjects/GameConstants", order =  1)]
+public  class GameConstants : ScriptableObject
+{
+	 public int maxLives;
+
+    // Mario's movement
+    public int speed;
+    public int maxSpeed;
+    public int upSpeed;
+    public int deathImpulse;
+    public Vector3 marioStartingPosition;
+
+    // Goomba's movement
+    public float goombaPatrolTime;
+    public float goombaMaxOffset;
+
+    //Score tracking
+    public int lastSessionScore;
+    public int highScore;
+}
