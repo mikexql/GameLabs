71311fe1c5f090b04750586f4e43d047045311b3
EMKwasam <samuelkwa1@gmail.com>
2025-10-17 00:12:53 +0800

    Tentatively completed Main Menu + loading scenes

--- Assets/Scripts/HUDManager.cs
+++ Assets/Scripts/HUDManager.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class HUDManager : MonoBehaviour
 {
     public GameObject gameOverCanvas;
     public GameObject inGameCanvas;
+    public GameObject mainMenuCanvas;
     public TextMeshProUGUI finalScoreText;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI mainMenuScoreText;
+
+    public Button startButton;
+    public Button resetLastScoreButton;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        if (startButton != null)
+        {
+            startButton.onClick.AddListener(OnStartButton);
+        }
+        if (resetLastScoreButton != null)
+        {
+            resetLastScoreButton.onClick.AddListener(OnResetLastScoreButton);
+        }
     }
 
     // Update is called once per frame
@@ -34,6 +48,7 @@ public class HUDManager : MonoBehaviour
     {   
         Debug.Log("HUD Manager: Game Start!");
         gameOverCanvas.SetActive(false);
+        mainMenuCanvas.SetActive(false);
         inGameCanvas.SetActive(true);
     }
 
@@ -45,9 +60,49 @@ public class HUDManager : MonoBehaviour
     }
 
     public void GameOver()
-    {   
+    {
         Debug.Log("HUD Manager: Game Over!");
         gameOverCanvas.SetActive(true);
         inGameCanvas.SetActive(false);
+        mainMenuCanvas.SetActive(false);
+        RefreshMenuScore();
     }
+
+    public void OnStartButton()
+    {
+        if (mainMenuCanvas != null) mainMenuCanvas.SetActive(false);
+        GameManager.instance.LoadSceneWithDelay("World 1-1", 1.0f, true);
+        
+    }
+
+    public void OnLoadingBackToMenu()
+    {
+        if (mainMenuCanvas != null) mainMenuCanvas.SetActive(true);
+        if (inGameCanvas != null) inGameCanvas.SetActive(false);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu");
+    }
+
+        public void OnResetLastScoreButton()
+    {
+        if (GameManager.instance != null)
+        {
+            GameManager.instance.ResetLastSessionScore();
+                RefreshMenuScore();
+        }
+    }
+
+        public void RefreshMenuScore()
+        {
+            if (mainMenuScoreText == null) return;
+
+            int last = 0;
+            if (GameManager.instance != null)
+            {
+                last = GameManager.instance.lastSessionScore;
+            }
+
+            mainMenuScoreText.text = "Last Score: " + last.ToString();
+        }
+    
+
 }
